1fa1f2f7943192ce49ef06ee5c6efc3e0db2257b
justzero <gb.justzero@gmail.com>
2012-11-07 11:49:22 +0800

    fix config

--- install.bat
+++ install.bat
@@ -1,10 +1,5 @@
-%USERPROFILE%/bin/elevate.exe fsutil hardlink create %USERPROFILE%\_vimrc %USERPROFILE%\.vim\vimrc
-%USERPROFILE%/bin/elevate.exe fsutil hardlink create %USERPROFILE%\_gvimrc %USERPROFILE%\.vim\gvimrc
-%USERPROFILE%/bin/junction.exe -q %USERPROFILE%\vimfiles %USERPROFILE%\.vim
-cd %USERPROFILE%\.vim
 git submodule update --init
-md ftplugin\javascript
-%USERPROFILE%/bin/junction.exe -q %USERPROFILE%\vimfiles\ftplugin\javascript\jslint %USERPROFILE%\vimfiles\bundle\jslint\ftplugin\javascript\jslint
-
+copy /y vimrc ..\_vimrc
+copy /y gvimrc ..\_gvimrc
 echo "Installation finished"
 pause
